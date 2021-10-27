8f10e91b0ee2535b74eb49abd96a9593bb235173
sergey1888888 <sergey1888888@gmail.com>
2021-10-27 12:54:30 +0300

    fix

--- src/hooks/useAdaptive.ts
+++ src/hooks/useAdaptive.ts
@@ -1,26 +1,14 @@
-import {useEffect, useLayoutEffect, useState} from "react";
+import {useLayoutEffect, useState} from "react";
 import useWindowSize from "./useWindowSize";
 
 export const useAdaptive = (): boolean => {
     const [isMobile, setIsMobile] = useState<boolean>(false);
-    const [initialized, setInitialized] = useState<boolean>(false);
     const {width: windowSize} = useWindowSize();
 
     useLayoutEffect(() => {
-        if (!initialized) {
-            if (windowSize && windowSize <= 1180) {
-                setIsMobile(true);
-            } else setIsMobile(false);
-            setInitialized(true);
-        }
-    }, [])
-
-    useEffect(() => {
-        if (initialized) {
-            if (windowSize && windowSize <= 1180) {
-                setIsMobile(true);
-            } else setIsMobile(false);
-        }
+        if (windowSize && windowSize <= 1180) {
+            setIsMobile(true);
+        } else setIsMobile(false);
     }, [windowSize])
 
     return isMobile;
